79e059bea51a69a4d958c5099ab36de846a5a353
Serg <s.ustin.92@mail.ru>
2023-12-05 13:09:09 +0300

    add revue by mentor

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,83 +1,116 @@
+import lombok.AccessLevel;
+import lombok.SneakyThrows;
+import lombok.experimental.FieldDefaults;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.internal.invocation.MockitoMethod;
+
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 class HorseTest {
     //test{Method}_Should{Do}_When{Condition}
 
+    String name = "Zephyr";
+    int speed = 10;
+    double distance = 10;
+
+    String exceptionMessageWhenNameIsNull = "Name cannot be null.";
+    String exceptionMessageWhenNameIsBlank = "Name cannot be blank.";
+    String exceptionMessageWhenSpeedIsNegative = "Speed cannot be negative.";
+    String exceptionMessageWhenDistanceIsNegative = "Distance cannot be negative.";
+
     @Test
     void testConstructor_ShouldThrowIllegalArgsEx_WhenArgsNameNull() {
-        assertThrows(IllegalArgumentException.class, () -> new Horse(null, 10));
+        assertThrows(IllegalArgumentException.class, () -> new Horse(null, speed));
     }
 
     @Test
     void testConstructor_ShouldTrowExTextMessage_WhenArgsNameNull() {
-        try {
-            new Horse(null, 10);
-            fail();
-        } catch (IllegalArgumentException e) {
-            assertEquals("Name cannot be null.", e.getMessage());
-        }
+        var exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, speed));
+        assertEquals(exceptionMessageWhenNameIsNull, exception.getMessage());
     }
 
-    // Проверить все тоже для остальных полей
-    // Для проверки isBlank, необходимо добавить @ParameterizedTest и @ValueSource
-    // Изучить уровни 3 4 5
-    // Проверить конструктор второго класса по аналогии
-
     @ParameterizedTest
-    @ValueSource(strings = "")
+    @ValueSource(strings = {"", " ", "\t", "\n", "\r", "\f"})
     void testConstructor_ShouldThrowIllegalArgsEx_WhenArgsNameBeBlank(String name) {
-        assertThrows(IllegalArgumentException.class, () -> new Horse(name, 10));
+        assertThrows(IllegalArgumentException.class, () -> new Horse(name, speed));
 
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "")
+    @ValueSource(strings = {"", " ", "\t", "\n", "\r", "\f"})
     void testConstructor_ShouldTrowExTextMessage_WhenArgsNameBeBlank(String name) {
-        try {
-            new Horse(name, 10);
-            fail();
-        } catch (IllegalArgumentException e) {
-            assertEquals("Name cannot be blank.", e.getMessage());
+        var exception = assertThrows(IllegalArgumentException.class, () -> new Horse(name, speed));
+        assertEquals(exceptionMessageWhenNameIsBlank, exception.getMessage());
+    }
+
+
+    @Test
+    void testConstructor_ShouldThrowIllegalArgsEx_WhenArgsSpeedLessThenZero() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(name, -speed));
+    }
+
+    @Test
+    void testConstructor_ShouldTrowExTextMessage_WhenArgsSpeedLessThenZero() {
+        var exception = assertThrows(IllegalArgumentException.class, () -> new Horse(name, -speed));
+        assertEquals(exceptionMessageWhenSpeedIsNegative, exception.getMessage());
+    }
+
+    @Test
+    void testConstructor_ShouldThrowIllegalArgsEx_WhenArgsDistanceLessThenZero() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(name, speed, -distance));
+    }
+
+    @Test
+    void testConstructor_ShouldTrowExTextMessage_WhenArgsDistanceLessThenZero() {
+        var exception = assertThrows(IllegalArgumentException.class, () ->
+                new Horse(name, speed, -distance));
+        assertEquals(exceptionMessageWhenDistanceIsNegative, exception.getMessage());
+    }
+
+    @Test
+    void testGetName_ShouldReturnName_WhenArgsConstructorIsName() {
+        assertEquals("Zephyr", new Horse(name, speed).getName());
+    }
+
+    @Test
+    @SneakyThrows
+    void testGetName_ShouldReturnName_WhenArgsConstructorIsNameUseReflection() {
+        String expectedName = "Zephyr";
+
+        Horse horse = new Horse(name, speed);
+        Field field = horse.getClass().getDeclaredField("name");
+        field.setAccessible(true);
+        String actualName = (String) field.get(horse);
+
+        assertEquals(expectedName, actualName);
+    }
+
+    // Дописать тесты на геттеры!
+    // На getDistance проверить когда в конструктор передано два параметра
+    // Проверить, что конструктор с двумя параметрами на третий возвращает 0
+    // Более подробное ТЗ в телеге!
+
+    // Проверить, что метод вызывает внутри метод getRandomDouble() с параметрами 0.2 и 0.9.
+    // Для этого нужно использовать MockedStatic и его метод verify();
+    // Проверить, что метод присваивает дистанции значение высчитанное по формуле:
+    // distance + speed * getRandomDouble(0.2, 0.9). Для этого нужно замокать getRandomDouble(),
+    // чтобы он возвращал определенные значения, которые нужно задать параметризовав тест.
+
+    @Test
+    void testMove() {
+        try (MockedStatic<Horse> mockedStatic = Mockito.mockStatic(Horse.class)) {
+            new Horse(name, speed, distance).move();
+            mockedStatic.verify(() -> Horse.getRandomDouble(0.2, 0.9));
         }
+
     }
 
-
-    @ParameterizedTest
-    @ValueSource(ints = {-1, -5, -10}) // Если добавить 0 то не работает??
-    void testConstructor_ShouldThrowIllegalArgsEx_WhenArgsSpeedLessThenZero(Integer speed) {
-        assertThrows(IllegalArgumentException.class, () -> new Horse("Horse", speed));
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {-1, -5, -10})
-    void testConstructor_ShouldTrowExTextMessage_WhenArgsSpeedLessThenZero(Integer speed) {
-        try {
-            new Horse("Horse", speed);
-            fail();
-        } catch (IllegalArgumentException e) {
-            assertEquals("Speed cannot be negative.", e.getMessage());
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {-1, -5, -10})
-    void testConstructor_ShouldThrowIllegalArgsEx_WhenArgsDistanceLessThenZero(Integer distance) {
-        assertThrows(IllegalArgumentException.class, () -> new Horse("Horse", 10, distance));
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {-1, -5, -10})
-    void testConstructor_ShouldTrowExTextMessage_WhenArgsDistanceLessThenZero(Integer distance) {
-        try {
-            new Horse("Horse", 10, distance);
-            fail();
-        } catch (IllegalArgumentException e) {
-            assertEquals("Distance cannot be negative.", e.getMessage());
-        }
-    }
 
 }
