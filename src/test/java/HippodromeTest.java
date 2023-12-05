e8a4d7b3376c22cbc6becb2f91b5cd998cf99da2
Serg <s.ustin.92@mail.ru>
2023-12-05 12:04:01 +0300

    add test for constructor

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,5 +1,49 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HippodromeTest {
+    //test{Method}_Should{Do}_When{Condition}
+
+    @Test
+    void testConstructor_ShouldThrowIllegalArgsEx_WhenArgsHorsesNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+    }
+
+    @Test
+    void testConstructor_ShouldTrowExTextMessage_WhenArgsHorsesNull() {
+        try {
+            new Hippodrome(null);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Horses cannot be null.", e.getMessage());
+        }
+
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    void testConstructor_ShouldThrowIllegalArgsEx_WhenArgsHorsesEmpty(List<Horse> horses) {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(horses));
+
+
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    void testConstructor_ShouldTrowExTextMessage_WhenArgsHorsesEmpty(List<Horse> horses) {
+        try {
+            new Hippodrome(horses);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Horses cannot be empty.", e.getMessage());
+        }
+
+    }
 
 }
--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -27,4 +27,57 @@ class HorseTest {
     // Изучить уровни 3 4 5
     // Проверить конструктор второго класса по аналогии
 
+    @ParameterizedTest
+    @ValueSource(strings = "")
+    void testConstructor_ShouldThrowIllegalArgsEx_WhenArgsNameBeBlank(String name) {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(name, 10));
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "")
+    void testConstructor_ShouldTrowExTextMessage_WhenArgsNameBeBlank(String name) {
+        try {
+            new Horse(name, 10);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Name cannot be blank.", e.getMessage());
+        }
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -5, -10}) // Если добавить 0 то не работает??
+    void testConstructor_ShouldThrowIllegalArgsEx_WhenArgsSpeedLessThenZero(Integer speed) {
+        assertThrows(IllegalArgumentException.class, () -> new Horse("Horse", speed));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -5, -10})
+    void testConstructor_ShouldTrowExTextMessage_WhenArgsSpeedLessThenZero(Integer speed) {
+        try {
+            new Horse("Horse", speed);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Speed cannot be negative.", e.getMessage());
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -5, -10})
+    void testConstructor_ShouldThrowIllegalArgsEx_WhenArgsDistanceLessThenZero(Integer distance) {
+        assertThrows(IllegalArgumentException.class, () -> new Horse("Horse", 10, distance));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -5, -10})
+    void testConstructor_ShouldTrowExTextMessage_WhenArgsDistanceLessThenZero(Integer distance) {
+        try {
+            new Horse("Horse", 10, distance);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Distance cannot be negative.", e.getMessage());
+        }
+    }
+
 }
