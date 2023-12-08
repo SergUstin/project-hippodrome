9fe53d09ce5d352dbcffa98aafaf39bddc32552d
Serg <s.ustin.92@mail.ru>
2023-12-08 13:49:11 +0300

    add tests for the Main class

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    @Test
+    @Timeout(value = 21)
+    @Disabled
+    void timeout() {
+        Main.main(null);
+    }
 
 }
